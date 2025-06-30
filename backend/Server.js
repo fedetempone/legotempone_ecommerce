4976ef72fbf71404fa1bb80aec8aaae2e061b836
fedetempone <fedetempone5@gmail.com>
2025-06-30 19:42:45 -0300

    Reverting Express static/dist routing changes — fix for direct route 404 on Render didn't work

--- backend/Server.js
+++ backend/Server.js
@@ -4,13 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
 import authRoutes from "./routes/auth.js";
-import path from "path";
-import { fileURLToPath } from "url";
 
 dotenv.config();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const port = process.env.PORT || 5000;
 
@@ -21,7 +16,7 @@ const allowedOrigins = [
   "https://ecommerce-legotempone.onrender.com",
 ];
 
-// configuro cors para que no me  bloquee las solicitudes
+// configuro cors para que no me bloquee las solicitudes
 app.use(
   cors({
     origin: function (origin, callback) {
@@ -37,7 +32,7 @@ app.use(
 
 app.use(express.json());
 
-// me conecto a mi base de datos en mongodb
+// conexion am i base de datos en MongoDB
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("Conectado a la base de datos"))
@@ -46,21 +41,12 @@ mongoose
     process.exit(1);
   });
 
-// Rutas API
+// creacion de rutas
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
 
-// las siguientes 3 lineas de codigo (app.use expres / app.get * req res / res.sendfile) son para que al actualizar el deploy de render
-// o para ingresar directamente a una ruta /products o /contact ol o que sea, no me tire 404
-// sirvo los archivos estaticos desde dist (esto es para ver si funciona en produccion)
-app.use(express.static(path.join(__dirname, "..", "dist")));
-
-// para cualquier ruta que no sea API, devuelvo index.html para que React Router maneje el enrutamiento (esto tmbn es para produccion)
-app.get(/^\/(?!api).*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
-});
-
-// prendo el server
+// prendo el servidor
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
 });
+
